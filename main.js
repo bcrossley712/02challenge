41e145092dbd0f068d75266c458b9e66128994ca
bcrossley712 <bcrossley712@gmail.com>
2022-02-12 20:18:37 -0700

    requirements met

--- main.js
+++ main.js
@@ -36,20 +36,28 @@ function mine() {
 
 function buyClickUpgrade(upgradeName) {
   let upgrade = clickUpgrades[upgradeName]
-  upgrade.quantity++
-  // console.log(upgrade);
-  document.getElementById('axe-count').innerText = clickUpgrades.pickaxes.quantity
-  document.getElementById('tnt-count').innerText = clickUpgrades.dynamite.quantity
-  // console.log(upgradedClick);
+  if (upgrade.price <= cheese) {
+    cheese -= upgrade.price
+    upgrade.quantity++
+    upgrade.price += (Math.floor(upgrade.price - upgrade.price * .7))
+    purchasedAlert()
+  } else {
+    declinedPurchaseAlert()
+  }
   drawStats()
 }
 
 function buyAutoUpgrade(upgradeName) {
   let upgrade = automaticUpgrades[upgradeName]
-  upgrade.quantity++
-  // console.log(upgrade);
-  document.getElementById('rover-count').innerText = automaticUpgrades.rovers.quantity
-  document.getElementById('harvest-count').innerText = automaticUpgrades.harvesters.quantity
+  if (upgrade.price <= cheese) {
+    cheese -= upgrade.price
+    upgrade.quantity++
+    upgrade.price += (Math.floor(upgrade.price - upgrade.price * .4))
+    // console.log(upgrade);
+    purchasedAlert()
+  } else {
+    declinedPurchaseAlert()
+  }
   drawStats()
 }
 
@@ -65,18 +73,67 @@ function runAutoUpgrades() {
     let upgrade = automaticUpgrades[key]
     cheese += (upgrade.quantity * upgrade.multiplier)
   }
-  document.getElementById('cheese-count').innerText = cheese
+  drawStats()
 }
 
 function drawStats() {
+  //NOTE Inventory tracker update
+  document.getElementById('cheese-count').innerText = cheese
+  document.getElementById('axe-count').innerText = clickUpgrades.pickaxes.quantity
+  document.getElementById('tnt-count').innerText = clickUpgrades.dynamite.quantity
+  document.getElementById('rover-count').innerText = automaticUpgrades.rovers.quantity
+  document.getElementById('harvest-count').innerText = automaticUpgrades.harvesters.quantity
+  //NOTE Stat tracker update
   let upgradedDynamite = (clickUpgrades.dynamite.multiplier * clickUpgrades.dynamite.quantity)
   let upgradedPickaxe = (clickUpgrades.pickaxes.multiplier * clickUpgrades.pickaxes.quantity)
   let upgradedRover = (automaticUpgrades.rovers.multiplier * automaticUpgrades.rovers.quantity)
   let upgradedHarvester = (automaticUpgrades.harvesters.multiplier * automaticUpgrades.harvesters.quantity)
-  // console.log(upgradedDynamite + upgradedPickaxe + 1);
   document.getElementById('click-collect').innerText = (upgradedDynamite + upgradedPickaxe + 1)
   document.getElementById('auto-collect').innerText = (upgradedRover + upgradedHarvester)
+  //NOTE Price of upgrade update
+  document.getElementById('pickaxes').innerText = (clickUpgrades.pickaxes.price)
+  document.getElementById('dynamite').innerText = (clickUpgrades.dynamite.price)
+  document.getElementById('rovers').innerText = (automaticUpgrades.rovers.price)
+  document.getElementById('harvesters').innerText = (automaticUpgrades.harvesters.price)
 }
 
+function purchasedAlert() {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
 
-let autoInterval = setInterval(runAutoUpgrades, 3000)
+  Toast.fire({
+    icon: 'success',
+    title: 'Purchased Upgrade'
+  })
+}
+
+function declinedPurchaseAlert() {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+
+  Toast.fire({
+    icon: 'error',
+    title: 'Not enough cheddar!'
+  })
+}
+
+drawStats()
+let autoInterval = setInterval(runAutoUpgrades, 2000)
